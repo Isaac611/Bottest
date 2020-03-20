549cbd0e13c9fb8531bda3dcf03c77b20ab3a261
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-21 02:58:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,38 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-var prefix = "$"
-
-var adminprefix = '$'
+var prefix = "w!" 
 
   client.on('message', msg => {
-
     if(msg.content === prefix + 'help') 
-
     
-
     msg.react('✅')
-
        .then(() => msg.react('✅')) 
-
        
-
   });
-
   
-
   
-
+  
   client.on("message", message => {
-
-    if (message.content === "$help") {
-
+    if (message.content === "help") {
      const embed = new Discord.RichEmbed() 
-
          .setColor("#FFB33F")
-
          .setThumbnail(message.author.avatarURL)
-
          .setDescription(`Help message`)
-
    message.author.sendEmbed(embed)
-
    
-
    }
-
    });
 
-client.login(process.env.TOKEN);
+// set game for bot
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Isaac`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Isaac ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`w!help`)
+});
 
+
+client.login(process.env.TOKEN);
