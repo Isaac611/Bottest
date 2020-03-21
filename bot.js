7d91e26c77b66a4983a29e990b8b48cdf036efb0
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-21 04:34:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "w!" 
+var prefix = "$" 
 
   client.on('message', msg => {
-    if(msg.content === prefix + 'help') 
+    if(msg.content === 'w!help') 
     
     msg.react('✅')
        .then(() => msg.react('✅')) 
@@ -21,7 +21,56 @@ var prefix = "w!"
    message.author.sendEmbed(embed)
    
    }
-   });
+   }); 
+   
+   client.on("message",message => {
+if(message.author.bot) return;
+if(!message.content.startsWith(prefix)) return;
+  if(message.content.startsWith(prefix + "background")){
+const mention = message.mentions.users.first()
+
+if(!mention) return console.log("") 
+let embed = new Discord.RichEmbed()
+.setColor("#FFB33F")
+.setAuthor(`${mention.username}#${mention.discriminator}`,`${mention.avatarURL}`) 
+.setTitle("Avatar Link")
+.setURL(`${mention.avatarURL}`)
+.setImage(`${mention.avatarURL}`)
+.setFooter(`Requested By ${message.author.tag}`,`${message.author.avatarURL}`)    
+    message.channel.send(embed)
+}
+})
+
+
+// show avatar users - and show avatar server
+client.on("message", message => {
+  if(message.author.bot) return;
+  if(!message.content.startsWith(prefix)) return;
+  if(message.content.startsWith(prefix + "background server")) {
+    let doma = new Discord.RichEmbed()
+    .setColor("#FFB33F")
+    .setAuthor(message.guild.name, message.guild.iconURL)
+    .setTitle("Avatar Link")
+    .setURL(message.guild.iconURL)
+    .setImage(message.guild.iconURL)
+    .setFooter(`Requested By ${message.author.tag}`, message.author.avatarURL)
+    message.channel.send(doma)
+  } else if(message.content.startsWith(prefix + "background")) {
+    let args = message.content.split(" ")[1]
+var avt = args || message.author.id;    
+    client.fetchUser(avt).then(user => {
+     avt = user;
+  let embed = new Discord.RichEmbed() 
+  .setColor("#FFB33F")
+  .setAuthor(`${avt.tag}`, avt.avatarURL)
+  .setTitle("Avatar Link")
+  .setURL(avt.avatarURL)
+  .setImage(avt.avatarURL)
+  .setFooter(`Requested By ${message.author.tag}`, message.author.avatarURL)
+  message.channel.send(embed) 
+    })
+  }
+})
 
 // set game for bot
 client.on('ready', () => {
