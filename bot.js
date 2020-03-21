7068237d8d96f09d018b9dc5c04b815481980e0f
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-21 18:38:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,10 +94,10 @@ client.on('message', function(message) {
     setInterval(function(){})
     let count = 0;
     let ecount = 0;  
-    var colors = ['FFB33F','3CDE95'];
+    var colors = ['#FFB33F','#3CDE95'];
     for(let c = 1; c <= 2; c++){ 
     message.guild.createRole({name:c,
-    color: `"#" + colors + '"' `})
+    color:colors[c]})
     }
     }
     }
