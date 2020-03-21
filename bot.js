b65cc02899f25a42f938decda1c20c3231997095
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-21 19:52:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,9 +107,10 @@ client.on('message', message=>{
     if (message.content === prefix + 'delete colors' || message.content === prefix + 'dcs'){
     if(!message.channel.guild) return; 
      for(let d = 1; d <= 2; d++){ 
-           message.channel.send('☑️ || Done deleted colors')
+           console.log(d)
+    guild.roles.find("name", d).delete();
       } 
-      
+      message.channel.send('☑️ || Done deleted colors')
    } 
   });
 
