c624d546f6565cafb21d40f40b2673c531a7089e
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-31 04:07:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,13 +144,13 @@ client.on('message', async message => {
 });
 
    
-   // kick and ban
+   // kick en
    client.on('message', message => {
   // Ignore messages that aren't from a guild
   if (!message.guild) return;
 
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('kick')) {
+  // If the message content starts with "kick"
+  if (message.content.startsWith(prefix + 'kick') && language == "en") {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
@@ -169,27 +169,73 @@ client.on('message', async message => {
           .kick('Optional reason that will display in the audit logs')
           .then(() => {
             // We let the message author know we were able to kick the person
-            message.reply(`Successfully kicked ${user.tag}`);
+            message.channel.send(`** ✅ The member was kicked ${user.tag} :crossed_swords: **`);
           })
           .catch(err => {
             // An error happened
             // This is generally due to the bot not being able to kick the member,
             // either due to missing permissions or role hierarchy
-            message.reply('I was unable to kick the member');
+            message.channel.send('** The member has not been kicked :rolling_eyes: **');
             // Log the error
             console.error(err);
           });
       } else {
         // The mentioned user isn't in this guild
-        message.reply("That user isn't in this guild!");
+       // message.reply("That user isn't in this guild!");
       }
       // Otherwise, if no user was mentioned
     } else {
-      message.reply("You didn't mention the user to kick!");
+      message.channel.send("**:thinking: You didn't mention the user to kick!**");
     }
   }
 });
 
+
+// kick ar
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "kick"
+  if (message.content.startsWith(prefix + 'kick') && language == "ar") {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.channel.send(`** ✅ تم ركل العضو بنجاح ${user.tag} :crossed_swords: **`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.channel.send('** لم يتم ركل العضو :rolling_eyes: **');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+       // message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.channel.send("**:thinking: لم تقم بذكر العضو أو منشنته**");
+    }
+  }
+});
    
    
 // show avatar users - and show avatar server en 
