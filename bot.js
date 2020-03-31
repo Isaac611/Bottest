617bd5a45dbec83313076d9fe0522c737aaf942e
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-31 15:57:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -336,7 +336,7 @@ client.on('message', message => {
 
   // If the message content starts with "kick"
   if (message.content.startsWith(prefix + 'ban') && language == "en" && ban == "activate") { 
-  	 	if (message.member.roles.some(role => role.name === `$banm`)) {
+  	 	if (message.member.roles.some(role => role.name === banm)) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
@@ -384,7 +384,7 @@ client.on('message', message => {
 
   // If the message content starts with "kick"
   if (message.content.startsWith(prefix + 'حظر') && language == "ar" && ban == "activate") { 
-  	if (message.member.roles.some(role => role.name === `$banm`)) {
+  	if (message.member.roles.some(role => role.name === banm)) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
