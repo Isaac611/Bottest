a2371f8f99721f1a94ac16a01553e679b4bd1005
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-03-31 03:37:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -145,41 +145,51 @@ client.on('message', async message => {
 
    
    // kick and ban
-   client.on("message", (message) => {
-   if (message.content.startsWith(prefix + "kick")) {
+   client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
 
-    if (!message.member.roles.find("name", "Admin"))
-        return;
-    // Easy way to get member object though mentions.
-    var member = message.mentions.members.first();
-    // Kick
-    member.kick().then((member) => {
-        // Successmessage
-        message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-    }).catch(() => {
-        // Failmessage
-        message.channel.send("Access Denied");
-    });
-}
-});
-client.on("message", (message) => {
-    if (message.content.startsWith(prefix + "ban")) {
-
-        if (!message.member.roles.find("name", "Admin"))
-            return;
-
-        // Easy way to get member object though mentions.
-        var member = message.mentions.members.first();
-        // ban
-        member.ban().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
-        }).catch(() => {
-            // Failmessage
-            message.channel.send("Access Denied");
-        });
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
     }
+  }
 });
+
    
    
 // show avatar users - and show avatar server en 
