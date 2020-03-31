bcd3ef51aea4ef2d5dc0795c6b1a5595b9290f55
Isaac611 <62386855+Isaac611@users.noreply.github.com>
2020-04-01 01:10:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -543,18 +543,17 @@ client.on('message', message => {
 client.on("message", message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-  if(message.guild.member(message.author.id).roles.find(role => role.name == saym)) && say == "activate" || message.member.hasPermission('ADMINISTRATOR') && say == "activate")
-      {
+      
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "say") {
+  if (command == "say" && message.guild.member(message.author.id).roles.find(role => role.name == saym)) && say == "activate" || message.member.hasPermission('ADMINISTRATOR') && say == "activate") {
 
     message.channel.sendMessage(args.join("  "));
     message.delete(); 
-  } 
+  
   }
 });  
 
